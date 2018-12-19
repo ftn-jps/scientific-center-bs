8eddc5b00cb042d7afcddd91e0ab69c73e24eaf4
BojanStipic <bojanstipic@gmail.com>
2018-12-19 17:15:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scientific center
+
+## Compiling from source
+
+```
+./mvnw package
+```
+
+## Usage
+
+```
+java -jar target/*.jar
+```
+
+Server listens on port 8090 by default. Use command line option `--server.port=` to change it.
---
+++ src/main/java/ftnjps/scientificcenter/ScientificCenterApplication.java
@@ -0,0 +1,12 @@
+package ftnjps.scientificcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificCenterApplication.class, args);
+	}
+}
