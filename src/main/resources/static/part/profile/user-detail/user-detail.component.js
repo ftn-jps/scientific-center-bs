4f36357aab60339d2fb0f716eb7a2039f04c272c
BojanStipic <bojanstipic@gmail.com>
2019-01-10 19:21:53 +0100

    Show map on profile page

--- src/main/resources/static/part/profile/user-detail/user-detail.component.js
+++ src/main/resources/static/part/profile/user-detail/user-detail.component.js
@@ -2,5 +2,16 @@
 
 angular.module('profile.userDetail')
 	.component('myUserDetail', {
-		templateUrl: '/part/profile/user-detail/user-detail.template.html'
+		templateUrl: '/part/profile/user-detail/user-detail.template.html',
+		controller: function($rootScope) {
+			// Leaflet map
+			const coordinates = $rootScope.user.location;
+			let map = L.map('map').setView(coordinates.split(','), 10);
+			// OpenStreetMap tiles
+			L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(map);
+			// Map marker
+			L.marker(coordinates.split(',')).addTo(map);
+		}
 	});
